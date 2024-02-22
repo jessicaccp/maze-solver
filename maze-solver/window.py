612a85149ec77594b43a31050792ebf8cfa883e1
Jessica Patricio <jessicacacau@gmail.com>
2024-02-22 18:49:07 -0300

    fix private methods' name

--- maze-solver/window.py
+++ maze-solver/window.py
@@ -9,29 +9,29 @@ class Window:
         self.__height = height
         self.__bg_color = "white"
         self.__is_running = False
-        self.create_root()
-        self.create_canvas()
+        self.__create_root()
+        self.__create_canvas()
 
-    def create_root(self):
+    def __create_root(self):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root.protocol("WM_DELETE_WINDOW", self.__close)
 
-    def create_canvas(self):
+    def __create_canvas(self):
         self.__canvas = Canvas(
             self.__root, bg=self.__bg_color, width=self.__width, height=self.__height)
         self.__canvas.pack(fill=BOTH, expand=1)
 
-    def redraw(self):
+    def __redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
 
     def wait_for_close(self):
         self.__is_running = True
         while self.__is_running:
-            self.redraw()
+            self.__redraw()
 
-    def close(self):
+    def __close(self):
         self.__is_running = False
 
     def draw_line(self, line, fill_color="black"):
