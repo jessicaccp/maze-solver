7265443db86fb07108c0bd556f787806f0f71ea3
Jessica Patricio <jessicacacau@gmail.com>
2024-02-22 18:45:20 -0300

    add annotations

--- maze-solver/line.py
+++ maze-solver/line.py
@@ -3,11 +3,14 @@ from point import Point
 
 
 class Line:
-    def __init__(self, point1, point2):
+    __point1: Point
+    __point2: Point
+
+    def __init__(self, point1: Point, point2: Point):
         self.__point1 = point1
         self.__point2 = point2
 
-    def draw(self, canvas, fill_color):
+    def draw(self, canvas: Canvas, fill_color: str) -> None:
         canvas.create_line(self.__point1.x, self.__point1.y,
                            self.__point2.x, self.__point2.y, fill=fill_color, width=2)
         canvas.pack(fill=BOTH, expand=1)
--- maze-solver/point.py
+++ maze-solver/point.py
@@ -1,4 +1,7 @@
 class Point:
-    def __init__(self, x, y):
+    x: float
+    y: float
+
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
