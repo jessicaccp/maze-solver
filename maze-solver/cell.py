8ef3d5a959476f988d7fd3e5f6815c4b3bd8a828
Jessica Patricio <jessicacacau@gmail.com>
2024-02-22 18:37:58 -0300

    add annotations and remove get_middle

--- maze-solver/cell.py
+++ maze-solver/cell.py
@@ -4,7 +4,19 @@ from line import Line
 
 
 class Cell:
-    def __init__(self, point1, point2, window):
+    has_top_wall: bool
+    has_right_wall: bool
+    has_bottom_wall: bool
+    has_left_wall: bool
+    __x1: float
+    __x2: float
+    __x3: float
+    __y1: float
+    __y2: float
+    __y3: float
+    __window: Window
+
+    def __init__(self, point1: Point, point2: Point, window: Window):
         self.has_top_wall = True
         self.has_right_wall = True
         self.has_bottom_wall = True
@@ -12,13 +24,15 @@ class Cell:
 
         self.__x1 = min(point1.x, point2.x)
         self.__x2 = max(point1.x, point2.x)
+        self.__x3 = self.__x1 + (self.__x2 - self.__x1) / 2
+
         self.__y1 = min(point1.y, point2.y)
         self.__y2 = max(point1.y, point2.y)
+        self.__y3 = self.__y1 + (self.__y2 - self.__y1) / 2
 
         self.__window = window
-        self.draw()
 
-    def draw(self):
+    def draw(self) -> None:
         if self.has_top_wall:
             self.__window.draw_line(Line(
                 Point(self.__x1, self.__y1),
@@ -39,21 +53,12 @@ class Cell:
                 Point(self.__x1, self.__y1),
                 Point(self.__x1, self.__y2)))
 
-    def get_middle(self, option):
-        match option:
-            case "x":
-                return self.__x1 + (self.__x2 - self.__x1) / 2
-            case "y":
-                return self.__y1 + (self.__y2 - self.__y1) / 2
-            case _:
-                raise ValueError
-
-    def draw_move(self, cell, undo=False):
+    def draw_move(self, cell: 'Cell', undo: bool = False) -> None:
         if undo:
             color = "gray"
         else:
             color = "red"
 
         self.__window.draw_line(Line(
-            Point(self.get_middle("x"), self.get_middle("y")),
-            Point(cell.get_middle("x"), cell.get_middle("y"))), color)
+            Point(self.__x3, self.__y3),
+            Point(cell.__x3, cell.__y3)), color)
